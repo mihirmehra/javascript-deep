5858e4d317c28a8b19fc2874b935009755f31bc9
mihirmehra <mihirmehrajio@gmail.com>
2025-06-06 16:50:30 +0530

    01_veriables

---
+++ 01_Basics/01_Veriables.js
@@ -0,0 +1,14 @@
+const accountID = 12345
+let accountName = "mihir"
+var accountEmail = "mihir@gmail.com"
+accountCity = "Delhi"
+
+// accountID = 123456 // not allowed
+
+accountName = "mihhir"
+accountEmail = "mihirrrr@gmail.com"
+accountCity = "Deeeelhi"
+
+console.log(accountID);
+
+console.table([accountID,accountName,accountEmail,accountCity]);
