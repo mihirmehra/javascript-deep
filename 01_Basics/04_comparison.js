310a31784edd8cbd645bd8b7f2dbc2fe632ef077
mihirmehra <49902630+mihirmehra@users.noreply.github.com>
2025-06-07 06:27:06 +0000

    comparison operators (><=) and equality operators (===, ==)

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 === 1);
+// console.log(2 !== 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0); // null is considered equal to 0 in loose equality, but not greater or less than 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+
+// === // strict equality - checks value and type
+// == // loose equality - checks value only
+
+// comparison operators (><=) and equality operators (===, ==) work differently 
