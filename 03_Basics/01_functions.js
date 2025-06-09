5e627cc3c1c90d77bbd484e6737de875520c86d6
mihirmehra <mihirmehrajio@gmail.com>
2025-06-09 11:19:20 +0530

    function 2

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -7,11 +7,11 @@ function add(num1,num2){
     return num1 + num2
 }
 
-add(1,2)
+// add(1,2)
 
-const res = add(9,5)
+// const res = add(9,5)
 
-console.log(res)
+// console.log(res)
 
 
 function loginUserMessage(username){
@@ -23,4 +23,33 @@ function loginUserMessage(username){
     }
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+// function calculateCartPrice(...num1){ //rest operator
+//     return num1
+// }
+function calculateCartPrice(val1, val2,...num1){ //rest operator
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,200))
+
+const user = {
+    username: "mihir",
+    price: 3000
+}
+
+function handleObj(obj){
+    console.log(`username: ${obj.username} price: ${obj.price}`)
+}
+
+
+handleObj(user)
+
+const points = [1,2,4,5,3,5]
+
+function handleArr(arr){
+    return arr[1]
+}
+
+console.log(handleArr(points))
