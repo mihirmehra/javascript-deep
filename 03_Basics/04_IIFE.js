ca2520173adadee29e41b5da53b69a32e0815b1f
mihirmehra <mihirmehrajio@gmail.com>
2025-06-09 14:32:41 +0530

    iife

--- 03_Basics/04_IIFE.js
+++ 03_Basics/04_IIFE.js
@@ -1,1 +1,12 @@
 // Immediately Invoked Function Expressions (IIFE)
+// we use iife to protect the code form the global polution
+
+(function chai(){
+    // named IIFE
+    console.log("db conneted");
+})(); // the semi colan is most important to end the invoke of a iife otherwise the code below will ot work
+
+((name) => {
+    // unnamed IIFE
+    console.log(`db conneted ${name}`);
+})("mihir");
